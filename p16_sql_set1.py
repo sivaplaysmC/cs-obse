d7dbc5cb1ef3c09f2beedf72a8cd8ab8aaefa871
sivaplaysmC <sivaram.asdf@gmail.com>
2022-05-28 19:40:27 +0530

    Added 16th and 17th program ^_^

--- p16_sql_set1.py
+++ p16_sql_set1.py
@@ -4,31 +4,31 @@ if mc.is_connected()==False:
     print("Error...")
 cr=mc.cursor()
 
-sql="""create table if not exists ITEMS(
-ItemNo integer not null primary key,
-Name varchar(25) not null,
-Dcode integer,
-Qty integer,
-UnitPrice integer,
-StockDate date);"""
+sql="""create table if not exists CARDEN(
+Ccode integer not null primary key,
+CarName varchar(25) not null,
+Make varchar(25),
+Color varchar(10),
+Capacity integer,
+Charges integer);"""
 cr.execute(sql)
 mc.commit()
 print("Table Created....")
 
 def insertion():
-    itemno=int(input("Enter the item no:"))
-    name=input("Enter the item name:")
-    dcode=int(input("Enter dcode:"))
-    qty=int(input("Enter the quantity:"))
-    unitprice=int(input("Enter the unitprice:"))
-    sd=input("Enter the stock date:")
-    sql="insert into ITEMS values({},'{}',{},{},{},'{}')".format(itemno,name,dcode,qty,unitprice,sd)
+    ccode=int(input("Enter the ccode:"))
+    cname=input("Enter the car name:")
+    make=input("Enter make:")
+    color=input("Enter color:")
+    capacity=int(input("Enter the capacity:"))
+    charges=int(input("Enter the charges:"))
+    sql="insert into CARDEN values({},'{}','{}','{}',{},{})".format(ccode,cname,make,color,capacity,charges)
     cr.execute(sql)
     mc.commit()
     print("Record inserted....")
     
 def query1():
-    sql="select Name from ITEMS where year(StockDate)=2010;"
+    sql="select CarName from CARDEN where Color='SILVER';"
     cr.execute(sql)
     data=cr.fetchall()
     flag=False
@@ -38,7 +38,10 @@ def query1():
     if flag==False:
         print("No record found")
 def query2():
-    sql="select ItemNo,Name from ITEMS where UnitPrice>10;"
+    sql="update CARDEN set charges=charges+10 where capacity>3;"
+    cr.execute(sql)
+    print("Record updated!")
+    sql="select * from CARDEN;"
     cr.execute(sql)
     data=cr.fetchall()
     flag=False
@@ -46,11 +49,9 @@ def query2():
         print(i)
         flag=True
     if flag==False:
-        print("No record found") ; 
-
-
+        print("No record found")
 def query3():
-    sql="select sum(Qty),Dcode from ITEMS group by Dcode;"
+    sql="select CarName,Capacity from CARDEN order by Capacity desc;"
     cr.execute(sql)
     data=cr.fetchall()
     flag=False
--- p17_sql_set2.py
+++ p17_sql_set2.py
@@ -4,31 +4,29 @@ if mc.is_connected()==False:
     print("Error...")
 cr=mc.cursor()
 
-sql="""create table if not exists ITEMS(
-ItemNo integer not null primary key,
-Name varchar(25) not null,
-Dcode integer,
-Qty integer,
-UnitPrice integer,
-StockDate date);"""
+sql="""create table if not exists SPORTS(
+Stud_no int not null primary key,
+Class int not null,
+name varchar(25),
+game1 varchar(30),
+grade1 varchar(5));"""
 cr.execute(sql)
 mc.commit()
 print("Table Created....")
 
 def insertion():
-    itemno=int(input("Enter the item no:"))
-    name=input("Enter the item name:")
-    dcode=int(input("Enter dcode:"))
-    qty=int(input("Enter the quantity:"))
-    unitprice=int(input("Enter the unitprice:"))
-    sd=input("Enter the stock date:")
-    sql="insert into ITEMS values({},'{}',{},{},{},'{}')".format(itemno,name,dcode,qty,unitprice,sd)
+    Stud_no=int(input("Enter the student no:"))
+    Class=int(input("Enter the class:"))
+    name=input("Enter name:")
+    game1=input("Enter game name:")
+    grade1=input("Enter grade:")
+    sql="insert into SPORTS values({},{},'{}','{}','{}')".format(Stud_no,Class,name,game1,grade1)
     cr.execute(sql)
     mc.commit()
     print("Record inserted....")
     
 def query1():
-    sql="select Name from ITEMS where year(StockDate)=2010;"
+    sql="Select name from SPORTS where game1='swimming';"
     cr.execute(sql)
     data=cr.fetchall()
     flag=False
@@ -38,7 +36,10 @@ def query1():
     if flag==False:
         print("No record found")
 def query2():
-    sql="select ItemNo,Name from ITEMS where UnitPrice>10;"
+    sql="Delete from SPORTS where class=10;"
+    cr.execute(sql)
+    print("The new record is:")
+    sql="select * from SPORTS;"
     cr.execute(sql)
     data=cr.fetchall()
     flag=False
@@ -46,11 +47,9 @@ def query2():
         print(i)
         flag=True
     if flag==False:
-        print("No record found") ; 
-
-
+        print("No record found")
 def query3():
-    sql="select sum(Qty),Dcode from ITEMS group by Dcode;"
+    sql="Select count(*) , class from SPORTS group by class;"
     cr.execute(sql)
     data=cr.fetchall()
     flag=False
